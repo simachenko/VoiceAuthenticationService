1410bcdb5249493d38e134ebc849e97b3c2604e1
simachenko <andrewsimachenko@outlook.com>
2022-10-06 19:47:27 +0300

    add docker profile

--- Diploma.MicroServices.WebSearch/Controllers/WebSearchController.cs
+++ Diploma.MicroServices.WebSearch/Controllers/WebSearchController.cs
@@ -3,6 +3,9 @@ using Diploma.DTO;
 using Diploma.DTO.CommandProcessing;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Diploma.MicroServices.WebSearch.Controllers
@@ -28,6 +31,39 @@ namespace Diploma.MicroServices.WebSearch.Controllers
 				return Forbid();
 			}
 			return Ok(new ActionResultDto<string>(command.Command));
+		}
+
+		[HttpPost]
+		[Route("Search")]
+		public async Task<ActionResult> Test(StringCommandDto command)
+		{
+
+			var list = new List<ModelDto>() { new ModelDto(1), new ModelDto(2), new ModelDto(3), new ModelDto(3), new ModelDto(), new ModelDto() };
+			var list2 = new List<ModelDto>() { new ModelDto(1), new ModelDto(2), new ModelDto(3), new ModelDto(4), new ModelDto(), new ModelDto() };
+
+			var zip = list.Zip(list2);
+			return Ok(new ActionResultDto<string>(command.Command));
+		}
+
+
+		private class ModelDto
+		{
+			private readonly Random _random = new Random(DateTime.Now.Millisecond);
+			public string Name { get; set; }
+			public int Id { get; set; }
+
+			public ModelDto()
+			{
+				Id = _random.Next(0, 100);
+				Name = _random.Next().ToString();
+			}
+
+			public ModelDto(int id)
+			{
+				Id = id;
+				Name = id.ToString();
+			}
+
 		}
 	}
 }
